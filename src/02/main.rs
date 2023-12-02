5681afc2082d6036175cc5d0a29e12aef22f6aa3
Jordi Been <jordi.been@ridecheck.app>
2023-12-02 15:05:08 +0100

    day 02 part 2

--- src/02/main.rs
+++ src/02/main.rs
@@ -6,6 +6,7 @@ fn main() {
     println!("Day 02");
     let input: String = fs::read_to_string("./src/02/input.txt").expect("File should exist");
     pt1(&input);
+    pt2(&input);
 }
 
 fn pt1(input: &String) -> () {
@@ -51,4 +52,35 @@ fn pt1(input: &String) -> () {
     });
 
     println!("Part 1 finished in {:?}. Answer: {}", start.elapsed(), sum);
+}
+
+fn pt2(input: &String) -> () {
+    println!("Part 2 starting..");
+    let start: Instant = Instant::now();
+
+    let mut sum = 0;
+    input.lines().for_each(|line: &str| {
+        let (_, cubes) = line.split_once(": ").unwrap();
+
+        let reds: Vec<i32> = Regex::new(r"(?<red>\d+) red")
+            .unwrap()
+            .find_iter(cubes)
+            .filter_map(|c| c.as_str().replace(" red", "").parse::<i32>().ok())
+            .collect();
+        let greens: Vec<i32> = Regex::new(r"(?<green>\d+) green")
+            .unwrap()
+            .find_iter(cubes)
+            .filter_map(|c| c.as_str().replace(" green", "").parse::<i32>().ok())
+            .collect();
+        let blues: Vec<i32> = Regex::new(r"(?<blue>\d+) blue")
+            .unwrap()
+            .find_iter(cubes)
+            .filter_map(|c| c.as_str().replace(" blue", "").parse::<i32>().ok())
+            .collect();
+
+        sum +=
+            reds.iter().max().unwrap() * greens.iter().max().unwrap() * blues.iter().max().unwrap();
+    });
+
+    println!("Part 2 finished in {:?}. Answer: {}", start.elapsed(), sum);
 }
