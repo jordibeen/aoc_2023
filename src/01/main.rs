458c298754493f7324df50ec6b8dc79fd2cae6e7
Jordi Been <jordi.been@ridecheck.app>
2023-12-02 17:32:10 +0100

    day 01 using reversed regex and folds

--- src/01/main.rs
+++ src/01/main.rs
@@ -13,56 +13,61 @@ fn pt1(input: &String) -> () {
     println!("Part 1 starting..");
     let start: Instant = Instant::now();
 
-    let mut calibration_value: i32 = 0;
-    input.lines().for_each(|line: &str| {
+    let sum = input.lines().into_iter().fold(0, |acc, line| {
         let digits: Vec<String> = Regex::new(r"\d")
             .unwrap()
             .find_iter(line)
             .map(|m| m.as_str().to_owned())
             .collect();
-        let combined_str = digits.first().unwrap().to_owned() + digits.last().unwrap();
-        calibration_value += combined_str.parse::<i32>().unwrap();
+
+        acc + (digits.first().unwrap().to_owned() + digits.last().unwrap())
+            .parse::<i32>()
+            .unwrap()
     });
 
-    println!(
-        "Part 1 finished in {:?}. Answer: {}",
-        start.elapsed(),
-        calibration_value
-    );
+    println!("Part 1 finished in {:?}. Answer: {}", start.elapsed(), sum);
 }
 
 fn pt2(input: &String) -> () {
     println!("Part 2 starting..");
     let start = Instant::now();
-    let replacements = [
-        ("one", "o1e"),
-        ("two", "t2o"),
-        ("three", "t3e"),
-        ("four", "f4r"),
-        ("five", "f5e"),
-        ("six", "s6x"),
-        ("seven", "s7n"),
-        ("eight", "e8t"),
-        ("nine", "n9e"),
-    ];
 
-    let mut calibration_value: i32 = 0;
-    input.lines().for_each(|line| {
-        let mut replacement_line = line.to_owned();
-        for (s, r) in replacements {
-            replacement_line = replacement_line.replace(s, r);
-        }
-        let digits: Vec<String> = Regex::new(r"\d")
+    let sum = input.lines().into_iter().fold(0, |acc, line| {
+        let rev_line = line.chars().rev().collect::<String>();
+        let first = Regex::new(r"\d|one|two|three|four|five|six|seven|eight|nine")
             .unwrap()
-            .find_iter(&replacement_line)
-            .map(|m| m.as_str().to_owned())
-            .collect();
-        let combined_str = digits.first().unwrap().to_owned() + digits.last().unwrap();
-        calibration_value += combined_str.parse::<i32>().unwrap();
+            .find(line)
+            .map(|x| match x.as_str() {
+                "one" => "1",
+                "two" => "2",
+                "three" => "3",
+                "four" => "4",
+                "five" => "5",
+                "six" => "6",
+                "seven" => "7",
+                "eight" => "8",
+                "nine" => "9",
+                _ => x.as_str(),
+            })
+            .unwrap();
+        let last = Regex::new(r"\d|enin|thgie|neves|xis|evif|ruof|eerht|owt|eno")
+            .unwrap()
+            .find(&rev_line)
+            .map(|x| match x.as_str() {
+                "eno" => "1",
+                "owt" => "2",
+                "eerht" => "3",
+                "ruof" => "4",
+                "evif" => "5",
+                "xis" => "6",
+                "neves" => "7",
+                "thgie" => "8",
+                "enin" => "9",
+                _ => x.as_str(),
+            })
+            .unwrap();
+        acc + (first.to_owned() + last).parse::<i32>().unwrap()
     });
-    println!(
-        "Part 2 finished in {:?}. Answer: {}",
-        start.elapsed(),
-        calibration_value
-    )
+
+    println!("Part 2 finished in {:?}. Answer: {}", start.elapsed(), sum)
 }
