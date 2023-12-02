35224b9575aa14d4a0c4f1feebc7c8d7a9c1b32b
Jordi Been <jordi.been@ridecheck.app>
2023-12-02 13:50:50 +0100

    day 02 part 1

---
+++ src/02/main.rs
@@ -0,0 +1,54 @@
+use regex::Regex;
+use std::fs;
+use std::time::Instant;
+
+fn main() {
+    println!("Day 02");
+    let input: String = fs::read_to_string("./src/02/input.txt").expect("File should exist");
+    pt1(&input);
+}
+
+fn pt1(input: &String) -> () {
+    println!("Part 1 starting..");
+    let start: Instant = Instant::now();
+
+    let mut sum = 0;
+    input.lines().for_each(|line: &str| {
+        let (game, cubes) = line.split_once(": ").unwrap();
+
+        let game_id: i32 = Regex::new(r"Game (?P<id>\d+)")
+            .unwrap()
+            .captures(game)
+            .unwrap()
+            .name("id")
+            .unwrap()
+            .as_str()
+            .parse::<i32>()
+            .unwrap();
+
+        let reds: Vec<i32> = Regex::new(r"(?<red>\d+) red")
+            .unwrap()
+            .find_iter(cubes)
+            .filter_map(|c| c.as_str().replace(" red", "").parse::<i32>().ok())
+            .collect();
+        let greens: Vec<i32> = Regex::new(r"(?<green>\d+) green")
+            .unwrap()
+            .find_iter(cubes)
+            .filter_map(|c| c.as_str().replace(" green", "").parse::<i32>().ok())
+            .collect();
+        let blues: Vec<i32> = Regex::new(r"(?<blue>\d+) blue")
+            .unwrap()
+            .find_iter(cubes)
+            .filter_map(|c| c.as_str().replace(" blue", "").parse::<i32>().ok())
+            .collect();
+
+        if reds.iter().max().unwrap() <= &12
+            && greens.iter().max().unwrap() <= &13
+            && blues.iter().max().unwrap() <= &14
+        {
+            sum += game_id;
+        }
+    });
+
+    println!("Part 1 finished in {:?}. Answer: {}", start.elapsed(), sum);
+}
