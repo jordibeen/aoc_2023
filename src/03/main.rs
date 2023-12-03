615563a7b07fe05afe5767c61715866e4e6a4686
Jordi Been <jordi.been@ridecheck.app>
2023-12-03 22:39:51 +0100

    day 03 readability

--- src/03/main.rs
+++ src/03/main.rs
@@ -12,59 +12,59 @@ fn main() {
     let start: Instant = Instant::now();
 
     let mut part_numbers: Vec<(PartValue, Match)> = Vec::new();
-    let mut symbols: HashMap<Position, HashSet<PartValue>> = HashMap::new();
+    let mut gears: HashMap<Position, HashSet<PartValue>> = HashMap::new();
 
     input.lines().enumerate().for_each(|(row_i, line)| {
         Regex::new(r"\d+")
             .unwrap()
             .captures_iter(line)
             .for_each(|cap| {
-                if let Some(part_match) = cap.get(0) {
-                    part_numbers.push((row_i, part_match));
-                };
+                let part_number = cap.get(0).unwrap();
+                part_numbers.push((row_i, part_number));
             });
 
         Regex::new(r"[^\d|\.]")
             .unwrap()
             .find_iter(line)
             .for_each(|find| {
-                let pos = (find.start() as i32, row_i as i32);
-                symbols.insert(pos, HashSet::new());
+                let gear_pos = (find.start() as i32, row_i as i32);
+                gears.insert(gear_pos, HashSet::new());
             });
     });
 
     part_numbers.iter().for_each(|(row, m)| {
         (m.start()..m.end()).for_each(|x| {
-            let part_number_pos = (x as i32, row.to_owned() as i32);
+            let pos = (x as i32, *row as i32);
             vec![
-                (part_number_pos.0 - 1, part_number_pos.1),
-                (part_number_pos.0 - 1, part_number_pos.1 + 1),
-                (part_number_pos.0 - 1, part_number_pos.1 - 1),
-                (part_number_pos.0, part_number_pos.1 + 1),
-                (part_number_pos.0, part_number_pos.1 - 1),
-                (part_number_pos.0 + 1, part_number_pos.1),
-                (part_number_pos.0 + 1, part_number_pos.1 + 1),
-                (part_number_pos.0 + 1, part_number_pos.1 - 1),
+                (pos.0 - 1, pos.1),
+                (pos.0 - 1, pos.1 + 1),
+                (pos.0 - 1, pos.1 - 1),
+                (pos.0, pos.1 + 1),
+                (pos.0, pos.1 - 1),
+                (pos.0 + 1, pos.1),
+                (pos.0 + 1, pos.1 + 1),
+                (pos.0 + 1, pos.1 - 1),
             ]
             .iter()
             .for_each(|pos| {
-                if let Some(symbol) = symbols.get_mut(pos) {
-                    symbol.insert(m.as_str().parse::<PartValue>().unwrap());
+                if let Some(gear) = gears.get_mut(pos) {
+                    let part_value = m.as_str().parse::<PartValue>().unwrap();
+                    gear.insert(part_value);
                 }
             });
         });
     });
 
-    let pt1 = symbols
-        .iter()
-        .fold(0, |acc, (_, values)| acc + values.iter().sum::<PartValue>());
+    let pt1 = gears.iter().fold(0, |acc, (_, part_values)| {
+        acc + part_values.iter().sum::<PartValue>()
+    });
 
-    let pt2 = symbols.iter().fold(0, |acc, (_, vals)| {
-        if vals.len() > 1 {
-            let symbol_value = vals
+    let pt2 = gears.iter().fold(0, |acc, (_, part_values)| {
+        if part_values.len() > 1 {
+            let gear_value = part_values
                 .iter()
                 .fold(0, |acc, c| if acc == 0 { *c } else { acc * c });
-            acc + symbol_value
+            acc + gear_value
         } else {
             acc
         }
