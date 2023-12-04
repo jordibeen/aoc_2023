4123e16ec3716d7bb0dc12f2a1560d1d36d6ffdd
Jordi Been <jordi.been@ridecheck.app>
2023-12-04 11:07:58 +0100

    day 04 part 2

--- src/04/main.rs
+++ src/04/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 use std::time::Instant;
 
 fn main() {
@@ -6,6 +6,7 @@ fn main() {
     let input: &str = include_str!("./input.txt");
     let start: Instant = Instant::now();
     println!("Part 1: {}", pt1(&input));
+    println!("Part 2: {}", pt2(&input));
     println!("Execution time: {:.3?}", start.elapsed());
 }
 
@@ -36,6 +37,52 @@ fn pt1(input: &str) -> usize {
     sum
 }
 
+fn pt2(input: &str) -> usize {
+    let mut copies: HashMap<usize, usize> = HashMap::new();
+
+    input.lines().enumerate().for_each(|(line_i, line)| {
+        let card_number = line_i + 1;
+        let (_, numbers) = line.split_once(": ").unwrap();
+        let (winning_numbers_s, my_numbers_s) = numbers.split_once(" | ").unwrap();
+
+        let winning_numbers: HashSet<&str> = winning_numbers_s
+            .split(" ")
+            .filter(|x| !x.is_empty())
+            .collect();
+        let my_numbers: HashSet<&str> = my_numbers_s.split(" ").filter(|x| !x.is_empty()).collect();
+
+        let mut multiplier = 1;
+        if let Some(c) = copies.get(&card_number) {
+            multiplier = multiplier + c;
+        }
+        (0..multiplier).for_each(|_| {
+            winning_numbers
+                .intersection(&my_numbers)
+                .enumerate()
+                .for_each(|(i, _)| {
+                    let next_card_number = card_number + (i + 1);
+                    if let Some(win) = copies.get_mut(&next_card_number) {
+                        *win += 1;
+                    } else {
+                        copies.insert(next_card_number, 1);
+                    }
+                });
+        });
+
+        if let Some(card_number) = copies.get_mut(&card_number) {
+            *card_number += 1;
+        } else {
+            copies.insert(card_number, 1);
+        };
+    });
+
+    let sum = copies
+        .iter()
+        .fold(0, |acc, (_, copy_count)| acc + copy_count);
+
+    sum
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -45,5 +92,12 @@ mod tests {
         let input = include_str!("./example.txt");
         let result = pt1(&input);
         assert_eq!(result, 13);
+    }
+
+    #[test]
+    fn pt2_test() {
+        let input = include_str!("./example.txt");
+        let result = pt2(&input);
+        assert_eq!(result, 30);
     }
 }
