7126f7d758231f8bccfba3d204cf7e07b759e7d1
Jordi Been <jordi.been@ridecheck.app>
2023-12-09 12:26:57 +0100

    day 09 part 1

---
+++ src/09/main.rs
@@ -0,0 +1,77 @@
+use std::time::Instant;
+
+fn main() {
+    println!("--- Day 9: Mirage Maintenance ---");
+    let input: &str = include_str!("./input.txt");
+    let start: Instant = Instant::now();
+    println!("Part 1: {}", pt1(&input));
+    println!("Execution time: {:.3?}", start.elapsed());
+}
+
+fn pt1(input: &str) -> isize {
+    println!("{}", input);
+
+    let mut histories: Vec<Vec<isize>> = input
+        .lines()
+        .map(|line| {
+            line.split(" ")
+                .map(|value| value.parse::<isize>().unwrap())
+                .collect()
+        })
+        .collect();
+
+    // let mut differences: Vec<Vec<isize>> = Vec::new();
+    let sequences: Vec<Vec<Vec<isize>>> = histories
+        .iter()
+        .map(|history| {
+            let mut diff_sequences: Vec<Vec<isize>> = Vec::new();
+
+            let mut diff_sequence: Vec<isize> = history.clone();
+            while !diff_sequence.iter().all(|x| x == &0_isize) {
+                diff_sequence = diff_sequence
+                    .iter()
+                    .enumerate()
+                    .filter_map(|(i, value)| {
+                        if i < diff_sequence.len() - 1 {
+                            let next_value = diff_sequence[i + 1];
+                            let diff = next_value - value;
+                            Some(diff)
+                        } else {
+                            None
+                        }
+                    })
+                    .collect();
+                diff_sequences.push(diff_sequence.clone());
+            }
+
+            diff_sequences
+        })
+        .collect();
+
+    let extrapolations: Vec<isize> = histories
+        .iter_mut()
+        .zip(&sequences)
+        .map(|(history, diff_sequences)| {
+            let extrapolation = diff_sequences
+                .iter()
+                .rev()
+                .fold(0, |acc, sequence| acc + sequence.last().unwrap());
+
+            history.last().unwrap() + extrapolation
+        })
+        .collect();
+
+    extrapolations.iter().sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn pt1_test() {
+        let input = include_str!("./example.txt");
+        let result = pt1(&input);
+        assert_eq!(result, 114);
+    }
+}
