1eccdc6d0586db1d7b02b6f3208486127699bd94
Jordi Been <jordi.been@ridecheck.app>
2023-12-17 22:25:02 +0100

    day 15 part 2

--- src/15/main.rs
+++ src/15/main.rs
@@ -1,15 +1,59 @@
-use std::time::Instant;
+use std::{collections::HashMap, time::Instant};
 
 fn main() {
     println!("--- Day 15: Lens Library ---");
     let input: &str = include_str!("./input.txt");
     let start: Instant = Instant::now();
     println!("Part 1: {}", pt1(&input));
+    println!("Part 2: {}", pt2(&input));
     println!("Execution time: {:.3?}", start.elapsed());
 }
 
 fn pt1(input: &str) -> usize {
     input.split(",").map(|step| hash(step.trim())).sum()
+}
+
+fn pt2(input: &str) -> usize {
+    let mut boxes: HashMap<usize, Vec<&str>> = HashMap::new();
+
+    input.split(",").for_each(|step| {
+        let step: &str = step.trim();
+
+        if let Some((label, _)) = step.split_once("=") {
+            let box_number: usize = hash(label);
+            if let Some(b) = boxes.get_mut(&box_number) {
+                if let Some(i) = b.iter().position(|x| x.contains(label)) {
+                    b[i] = step;
+                } else {
+                    b.push(step);
+                }
+            } else {
+                boxes.insert(box_number, vec![step]);
+            }
+        }
+
+        if let Some((label, _)) = step.split_once("-") {
+            let box_number: usize = hash(label);
+            if let Some(b) = boxes.get_mut(&box_number) {
+                b.retain(|x| !x.contains(label));
+            }
+        }
+    });
+
+    let sum = boxes.iter().fold(0, |acc, (b, lenses)| {
+        acc + lenses
+            .iter()
+            .enumerate()
+            .map(|(i, lens)| {
+                let (_, focal_length) = lens.split_once("=").unwrap();
+                let lens_power = (b + 1) * (i + 1) * focal_length.parse::<usize>().unwrap();
+
+                lens_power
+            })
+            .sum::<usize>()
+    });
+
+    sum
 }
 
 fn hash(string: &str) -> usize {
@@ -34,5 +78,12 @@ mod tests {
         let input = include_str!("./example.txt");
         let result = pt1(&input);
         assert_eq!(result, 1320);
+    }
+
+    #[test]
+    fn pt2_test() {
+        let input = include_str!("./example.txt");
+        let result = pt2(&input);
+        assert_eq!(result, 145);
     }
 }
