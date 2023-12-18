358885295e82c7aaff9568432633830472859431
Jordi Been <jordi.been@ridecheck.app>
2023-12-18 01:51:03 +0100

    day 16 part 2

--- src/16/main.rs
+++ src/16/main.rs
@@ -6,18 +6,22 @@ use std::{
 type Coords = (isize, isize);
 type Direction = (isize, isize);
 type Beam = (Coords, Direction);
+type Occupations = HashMap<Coords, char>;
+type Grid = Vec<Vec<char>>;
 
 fn main() {
     println!("--- Day 16: The Floor Will Be Lava ---");
     let input: &str = include_str!("./input.txt");
     let start: Instant = Instant::now();
-    println!("Part 1: {}", pt1(&input));
+
+    let (occupations, grid) = parse(&input);
+    println!("Part 1: {}", pt1(&occupations));
+    println!("Part 2: {}", pt2(&occupations, &grid));
     println!("Execution time: {:.3?}", start.elapsed());
 }
 
-fn pt1(input: &str) -> usize {
-    let mut occupations: HashMap<Coords, char> = HashMap::new();
-    let mut energized: Vec<Coords> = Vec::new();
+fn parse(input: &str) -> (Occupations, Grid) {
+    let mut occupations: Occupations = HashMap::new();
 
     let grid: Vec<Vec<char>> = input
         .lines()
@@ -33,20 +37,40 @@ fn pt1(input: &str) -> usize {
         })
         .collect();
 
-    // Debug grid
-    println!("=============");
-    grid.iter().enumerate().for_each(|(y, row)| {
-        row.iter().enumerate().for_each(|(x, _)| {
-            print!("{}", occupations.get(&(y as isize, x as isize)).unwrap());
-        });
-        print!("\n");
-    });
-    println!("=============");
+    (occupations, grid)
+}
 
+fn pt1(occupations: &Occupations) -> usize {
+    enlighten(&occupations, ((0, -1), (0, 1)))
+}
+
+fn pt2(occupations: &Occupations, grid: &Grid) -> usize {
+    let mut initial_beams: HashMap<Beam, usize> = HashMap::new();
+
+    let max_y = grid.len();
+    let max_x = grid[0].len();
+    (0..max_y).for_each(|y| {
+        initial_beams.insert(((y as isize, -1), (0, 1)), 0);
+        initial_beams.insert(((y as isize, max_x as isize), (0, -1)), 0);
+    });
+    (0..max_x).for_each(|x| {
+        initial_beams.insert(((-1, x as isize), (1, 0)), 0);
+        initial_beams.insert(((max_y as isize, x as isize), (-1, 0)), 0);
+    });
+
+    initial_beams.iter_mut().for_each(|(initial_beam, score)| {
+        *score = enlighten(&occupations, *initial_beam);
+    });
+
+    *initial_beams.values().max().unwrap()
+}
+
+fn enlighten(occupations: &Occupations, starting_beam: Beam) -> usize {
+    let mut energized: Vec<Coords> = Vec::new();
     let mut beams: VecDeque<Beam> = VecDeque::new();
     let mut checked_beams: Vec<Beam> = Vec::new();
 
-    beams.push_front(((0, -1), (0, 1)));
+    beams.push_front(starting_beam);
 
     while let Some(beam) = beams.pop_front() {
         if checked_beams.contains(&beam) {
@@ -114,24 +138,8 @@ fn pt1(input: &str) -> usize {
                 _ => (),
             }
         }
-
         checked_beams.push(beam);
     }
-
-    println!("Energized");
-    grid.iter().enumerate().for_each(|(y, row)| {
-        row.iter().enumerate().for_each(|(x, _)| {
-            // print!("{}", occupations.get(&(y, x)).unwrap());
-            if energized.contains(&(y as isize, x as isize)) {
-                print!("#");
-            } else {
-                print!(".");
-            }
-        });
-        print!("\n");
-    });
-    println!("=============");
-
     energized.iter().count() - 1
 }
 
@@ -142,7 +150,16 @@ mod tests {
     #[test]
     fn pt1_test() {
         let input = include_str!("./example.txt");
-        let result = pt1(&input);
+        let (occupations, _) = parse(&input);
+        let result = pt1(&occupations);
         assert_eq!(result, 46);
+    }
+
+    #[test]
+    fn pt2_test() {
+        let input = include_str!("./example.txt");
+        let (occupations, grid) = parse(&input);
+        let result = pt2(&occupations, &grid);
+        assert_eq!(result, 51);
     }
 }
